73858471b174877d7183e6e20f17d88f30c73bba
Shannon Pieternella <shannonpieternella@gmail.com>
2020-04-17 16:58:40 +0200

    First commit

---
+++ src/utils/Settings.js
@@ -0,0 +1,13 @@
+export const RESOLUTION_ARR = {
+  "120p,120p_1": [160, 120, 15, 65],
+  "360p_4": [640, 360, 30, 600],
+  "480p_4": [640, 480, 30, 750],
+  "720p_3": [1280, 720, 30, 1710]
+};
+
+export const SHARE_ID = 1;
+
+export const APP_ID = "3e201b978c734f86bb1db7a4dbfdadd2";
+
+// Assign Token to null if you have not enabled app certificate
+export const Token = "0063e201b978c734f86bb1db7a4dbfdadd2IAA0UvJ9NtRqwTmEnGNimc+7YYUG1OcgyR9QETAuulpLETspIXIAAAAAEADOL/3oSfSZXgEAAQBH9Jle";
